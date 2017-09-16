aa3a8fe129c3d0b9fd380068a21927f5b592937a
Tihomir <tisho26bg@gmail.com>
2017-09-16 13:36:12 +0300

    редирект след логин на админ

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
+use Auth;
 class LoginController extends Controller
 {
     /*
@@ -19,7 +20,6 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
-
     /**
      * Where to redirect users after login.
      *
@@ -35,5 +35,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected  function authenticated(Request $request, $user)
+    {
+        if(Auth::user()->id==1){
+            return redirect()->route('admin');
+        }
+
     }
 }
