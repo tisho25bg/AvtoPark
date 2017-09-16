23de19096c68ed7bdae73339f3ccd904b97e963b
Yuzdhan Mehmedov <ynmehmedov@gmail.com>
2017-09-16 13:11:26 +0300

    Добавен контролер за админ

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Auth;
-
 class ProfileController extends Controller
 {
-    public function profile()
-    {
+	public function profile()
+	{
 
-        return view('users.admin.layouts.master-admin');
-    }
-
+		return view('home');
+	}
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+{
+	return view('welcome');
 });
 
 Auth::routes();
@@ -21,4 +22,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile/', 'ProfileController@profile');
 
-
+Route::get('/admin/', 'AdminController@index');
